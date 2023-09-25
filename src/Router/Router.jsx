35e35fdfa879c6e6313af9920cb938247f75a29c
Nashiuz Zaman <nashiuz.zaman@gmail.com>
2023-09-25 19:10:29 +0600

    Add 2 more routes to the website, donations and statistics

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -4,6 +4,22 @@ import { createBrowserRouter } from "react-router-dom";
 // components
 import Root from "../Components/Root/Root";
 
+// pages
+import Home from "../pages/Home/Home";
+import Donation from "../pages/Donation/Donation";
+import Statistics from "../pages/Statistics/Statistics";
+
 export const router = createBrowserRouter([
-  { path: "/", element: <Root></Root> },
+  {
+    path: "/",
+    element: <Root></Root>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      { path: "/donations", element: <Donation></Donation> },
+      { path: "/statistics", element: <Statistics></Statistics> },
+    ],
+  },
 ]);
