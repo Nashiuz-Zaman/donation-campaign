edef550855e7731b03b900af29c3d7e39c8ccd9d
Nashiuz Zaman <nashiuz.zaman@gmail.com>
2023-09-27 16:51:17 +0600

    Implement the see all button and its functionality

--- src/pages/DonationsPage/DonationsPage.jsx
+++ src/pages/DonationsPage/DonationsPage.jsx
@@ -1,5 +1,6 @@
 // react
 import { useLoaderData } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 // components
 import SavedDonations from "../../Components/SavedDonations/SavedDonations";
@@ -9,6 +10,8 @@ import Container from "../../Components/Container/Container";
 import useDonationContext from "../../hooks/useDonationContext";
 
 const DonationsPage = () => {
+  const [limitedView, setLimitedView] = useState(true);
+  const [displayDonations, setDisplayDonations] = useState([]);
   // get the loaded data
   const donations = useLoaderData();
   // extract the custom function to retreive data from local storage
@@ -21,10 +24,26 @@ const DonationsPage = () => {
     savedDonations.includes(donation.id)
   );
 
+  useEffect(() => {
+    if (limitedView) {
+      setDisplayDonations(donationsToShow.slice(0, 4));
+    } else {
+      setDisplayDonations(donationsToShow);
+    }
+  }, [limitedView, donationsToShow]);
+
   return (
     <div className="mt-20 mb-[9.625rem]">
       <Container>
-        <SavedDonations savedDonationsInfo={donationsToShow}></SavedDonations>
+        <SavedDonations savedDonationsInfo={displayDonations}></SavedDonations>
+        <button
+          className={`bg-[#009444] text-white rounded-default px-7 py-[0.875rem] font-semibold leading-[normal] w-max block mx-auto mt-10 ${
+            !limitedView && "hidden"
+          }`}
+          onClick={() => setLimitedView(false)}
+        >
+          See All
+        </button>
       </Container>
     </div>
   );
